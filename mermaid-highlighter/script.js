d11c329fd4c6ce9edc47c6a65548caa3d7cd995f
Explik <90565470+Explik@users.noreply.github.com>
2024-11-29 19:50:35 +0100

    Adding additional labels and enabling multiple colors (foreground and background)

--- mermaid-highlighter/script.js
+++ mermaid-highlighter/script.js
@@ -4,15 +4,31 @@ setTimeout(() => highlightLabels(), 100);
 function highlightLabels() {
     const mermaidElements = document.querySelectorAll('.mermaid');
     const INIT_LABEL = 'INIT_LABEL';
+    const CONDITION_LABEL = 'CONDITION_LABEL';
+    const STATEMENT_LABEL_1 = 'STATEMENT_LABEL_1';
+    const STATEMENT_LABEL_2 = 'STATEMENT_LABEL_2';
+    const INCREMENT_LABEL = 'INCREMENT_LABEL';
 
     mermaidElements.forEach(element => {
-        const labelElement = findLabeledElement(element, INIT_LABEL);
-        if (labelElement) {
-            // Do something with labelElement
-            console.log('Label found:', labelElement);
+        const initLabelElement = findLabeledElement(element, INIT_LABEL);
+        if (initLabelElement)
+            replaceLabelWithElement(element, INIT_LABEL, { text: 'Init', backgroundColor: '#ff6666' });
+        
+        const conditionLabelElement = findLabeledElement(element, CONDITION_LABEL);
+        if (conditionLabelElement)
+            replaceLabelWithElement(element, CONDITION_LABEL, { text: 'Condition', backgroundColor: '#ff6666' });
 
-            replaceLabelWithElement(element, INIT_LABEL, 'Init');
-        }
+        const statementLabelElement1 = findLabeledElement(element, STATEMENT_LABEL_1);
+        if (statementLabelElement1)
+            replaceLabelWithElement(element, STATEMENT_LABEL_1, { text: 'Statement 1', backgroundColor: '#66ff66' });
+
+        const statementLabelElement2 = findLabeledElement(element, STATEMENT_LABEL_2);
+        if (statementLabelElement2)
+            replaceLabelWithElement(element, STATEMENT_LABEL_2, { text: 'Statement 2', backgroundColor: '#66ff66' });
+
+        const incrementLabelElement = findLabeledElement(element, INCREMENT_LABEL);
+        if (incrementLabelElement)
+            replaceLabelWithElement(element, INCREMENT_LABEL, { text: 'Increment', backgroundColor: '#ff6666' });
     });
 }
 
@@ -35,15 +51,19 @@ function findLabeledElement(element, label) {
     return deepestElement ? deepestElement.element : null;
 }
 
-function replaceLabelWithElement(element, label, humanReadableLabel) {
+function replaceLabelWithElement(element, label, { text, textColor, backgroundColor }) {
+    text = text || label;
+    textColor = textColor || '#000';
+    backgroundColor = backgroundColor || '#fff';
+
     const labelElement = findLabeledElement(element, label);
     if (labelElement) {
         const newLabelElement = document.createElement('span');
-        newLabelElement.innerText = humanReadableLabel;
-        newLabelElement.style.backgroundColor = '#ffcc00';
+        newLabelElement.innerText = text;
+        newLabelElement.style.backgroundColor = backgroundColor;
         newLabelElement.style.borderRadius = '5px';
         newLabelElement.style.padding = '0px 5px';
-        newLabelElement.style.color = '#000';
+        newLabelElement.style.color = textColor;
         newLabelElement.style.fontWeight = 'bold';
         newLabelElement.style.display = 'inline-block';
 
